f702e523b1f362e275fe4b7782ff6a36224f771d
NgocTri <tringuyenngoc03@gmail.com>
2024-10-17 22:44:43 +0700

    3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import StoreContextProvide from "./context/StoreContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvide>
+      <App />
+    </StoreContextProvide>
   </BrowserRouter>
 );
